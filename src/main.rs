38037e20f05d25f2c966d7c7235acc172c96e428
Alex Saveau <saveau.alexandre@gmail.com>
2022-01-27 14:45:57 -0800

    Support going into full screen
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use bevy::{
     app::App,
     core_pipeline::ClearColor,
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
-    ecs::system::Commands,
+    ecs::system::{Commands, Res, ResMut},
+    input::{keyboard::KeyCode, Input},
     render::{camera::OrthographicCameraBundle, color::Color},
     ui::entity::UiCameraBundle,
-    window::WindowDescriptor,
+    window::{WindowDescriptor, WindowMode, Windows},
     DefaultPlugins,
 };
 use bevy_prototype_lyon::plugin::ShapePlugin;
@@ -34,7 +35,8 @@ fn main() {
     .add_plugin(ScreenDiagsPlugin)
     .add_plugin(ShapePlugin)
     .add_plugin(rectangles::RectanglesPlugin)
-    .add_startup_system(setup_cameras);
+    .add_startup_system(setup_cameras)
+    .add_system(full_screen_toggle);
 
     if cfg!(debug_assertions) {
         app.add_plugin(FrameTimeDiagnosticsPlugin::default())
@@ -47,4 +49,15 @@ fn main() {
 fn setup_cameras(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
+}
+
+fn full_screen_toggle(mut windows: ResMut<Windows>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.just_released(KeyCode::F) {
+        let window = windows.get_primary_mut().unwrap();
+        window.set_mode(if window.mode() == WindowMode::Windowed {
+            WindowMode::BorderlessFullscreen
+        } else {
+            WindowMode::Windowed
+        });
+    }
 }
