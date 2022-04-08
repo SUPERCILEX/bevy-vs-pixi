b918f2e2fa7d6b0359c7a9449e614ffd4ab9a808
Alex Saveau <saveau.alexandre@gmail.com>
2022-04-07 20:36:47 -0700

    Get rid of weealloc since it doesn't work
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,6 @@ use bevy_screen_diags::ScreenDiagsPlugin;
 
 mod rectangles;
 
-#[cfg(target_arch = "wasm32")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 fn main() {
     #[cfg(target_arch = "wasm32")]
     console_error_panic_hook::set_once();
